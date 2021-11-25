149989dad1ea5756454eb7ffedd77359051a936d
Sergiod3x <sergiod3x.@gmail.com>
2021-11-25 02:52:43 +0100

    Del ok

--- handler.js
+++ handler.js
@@ -83,6 +83,52 @@ module.exports = {
     };
     return response;
   },
+  create: async (event) => {
+
+    let bodyObj = {}
+    try {
+      bodyObj = JSON.parse(event.body)
+    } catch (error) {
+      console.log('Ci sono dei problemi nel recuperare le chiavi' + error)
+      const response = {
+        statusCode: 400,
+        body: JSON.stringify('Ci sono dei problemi nel recuperare le chiavi: ' + error)
+      };
+      return response;
+    }
+
+
+
+    var numberOne = bodyObj.numberOne;
+
+
+    const params = {
+      TableName: TABLE,
+      Item: {
+        numberOne: numberOne.toString()
+      },
+    };
+
+    try {
+      await DYNAMODB.put(params).promise();
+    } catch (error) {
+      console.log(error);
+      const response = {
+        statusCode: 500,
+        body: JSON.stringify("Errore imprevisto nel caricare le chiavi " + numberOne + " sul database: " + error),
+      };
+      return response;
+    }
+
+
+    const response = {
+
+      statusCode: 200,
+      body: JSON.stringify("Chiavi " + numberOne + " caricate sulla tabella " + TABLE),
+    };
+    return response;
+  },
+
   list: async (event) => {
 
     let scanParams = {
@@ -124,7 +170,7 @@ module.exports = {
       }
     }
 
-  
+
 
     let getResult = {}
     try {
@@ -138,12 +184,12 @@ module.exports = {
       return response;
     }
 
-    
-    
+
+
     if (getResult.Item === undefined) {
       const response = {
         statusCode: 404,
-        body: JSON.stringify("L'elemento non è presente nel database" ),
+        body: JSON.stringify("L'elemento non è presente nel database"),
       };
       return response;
     }
@@ -153,10 +199,109 @@ module.exports = {
     //   body: JSON.stringify("Errore imprevisto: Questi i parametri del DB :" + getParams.TableName),
     // };
     // return response;
-    
+
     return {
       statusCode: 200,
-      body: JSON.stringify("La chiave che cerchi è: "+getResult.Item.numberOne)
+      body: JSON.stringify("La chiave che cerchi è: " + getResult.Item.numberOne)
     }
   },
+  // update: async (event) => {
+  //   let bodyObj = {}
+  //   try {
+  //     bodyObj = JSON.parse(event.body)
+  //   } catch (error) {
+  //     console.log('Ci sono dei problemi nel recuperare le chiavi'+ error)
+  //     const response = {
+  //       statusCode: 400,
+  //       body: JSON.stringify('Ci sono dei problemi nel recuperare le chiavi: ' + error)
+  //     };
+  //     return response;
+  //   }
+  //   // if (typeof bodyObj.age === 'undefined') {
+  //   //   console.log('Missing parameters')
+  //   //   return {
+  //   //     statusCode: 400
+  //   //   }
+  //   // }
+
+  //   let updateParams = {
+  //     TableName: TABLE,
+  //     Key: {
+  //       numberOne: event.pathParameters.numberOne
+  //     },
+  //     UpdateExpression: 'set #age =:age',
+  //     ExpressionAttributeName: {
+  //       '#age': 'age'
+  //     },
+  //     ExpressionAttributeValues: {
+  //       ':age': bodyObj.age
+  //     },
+  //   }
+  //   try {
+  //     let dynamodb = new AWS.DynamoDB.DocumentClient()
+  //     dynamodb.update(updatetParams).promise()
+  //   } catch (updateError) {
+  //     console.log('There was a problem updatting the kitten')
+  //     console.log('updateParams', updateParams)
+  //     return {
+  //       statusCode: 500
+  //     }
+  //   }
+  //   if (updateResult.Items === null) {
+  //     return {
+  //       statusCode: 404
+  //     }
+  //   }
+  //   return {
+  //     statusCode: 200,
+
+  //   }
+  // },
+  delete: async (event) => {
+    let deleteParams = {
+      TableName: TABLE,
+      Key: {
+        numberOne: event.pathParameters.name.toString()
+      }
+    }
+
+    let getResult = {}
+    try {
+      getResult = await DYNAMODB.get(deleteParams).promise()
+    } catch (error) {
+      console.log('Ci sono problemi nella lettura del database: ' + error + " Questi i parametri del DB :" + deleteParams.TableName)
+      const response = {
+        statusCode: 500,
+        body: JSON.stringify("Errore imprevisto: " + error + " Questi i parametri del DB :" + deleteParams.TableName),
+      };
+      return response;
+    }
+
+
+
+    if (getResult.Item === undefined) {
+      const response = {
+        statusCode: 404,
+        body: JSON.stringify("L'elemento " + deleteParams.Key.numberOne + " non è presente nel database"),
+      };
+      return response;
+    }
+
+    let deleteResult = {}
+    try {
+      deleteResult = await DYNAMODB.delete(deleteParams).promise()
+    } catch (error) {
+      console.log("Ci sono problemi nell'eliminazione della chiave dal database: " + error)
+      const response = {
+        statusCode: 500,
+        body: JSON.stringify("Errore imprevisto: " + error + "Ci sono problemi nell'eliminazione della chiave dal database "),
+      };
+      return response;
+    }
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify("L'elemento " + deleteParams.Key.numberOne + " è stato eliminato dal database "),
+    };
+    return response;
+  }
 };
